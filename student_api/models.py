3b81836531d3ee68f1ba2470582139fd589b24e4
bibekbhandari <bhandaribibek27@gamil.com>
2024-12-30 16:34:14 +0545

    serilized

--- student_api/models.py
+++ student_api/models.py
@@ -6,3 +6,6 @@ class Students(models.Model):
     address=models.CharField(max_length=100)
     mobile=models.CharField(max_length=10)
     roll_number=models.IntegerField()
+
+    def __str__(self):
+        return self.first_name+""+self.last_name
