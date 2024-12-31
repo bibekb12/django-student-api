565b2a00c2fc03dfc2dcffa3b9d719a523e54458
bibekbhandari <bhandaribibek27@gamil.com>
2024-12-31 12:26:09 +0545

    pegination

--- student_api/views.py
+++ student_api/views.py
@@ -3,26 +3,33 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Students
 from .serializers import StudentSerializer
 
 class StudentView(APIView):
+    pagination_class = PageNumberPagination
     # def get(self,request, *args, **kwargs):
     #     result = Students.objects.all()
     #     serial = StudentSerializer(result, many= True)
     #     return Response({"status":"success","students":serial.data},status = status.HTTP_200_OK)
     
     def get(self,request,id=None):
-        
         if id:
             result = Students.objects.get(id=id)
             serial = StudentSerializer(result)
-            return Response({'success':"success","students":serial.data}, status= 200)
+            return Response({'success':"success","students":serial.data}, status=status.HTTP_200_OK)
         
         result = Students.objects.all()
-        serial = StudentSerializer(result, many= True)
-        return Response({"status":"suceess","students":serial.data}, status=200)
+        paginator = PageNumberPagination()
+        page = paginator.paginate_queryset(result, request)
+        if page is not None:
+            serializer = StudentSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        serializer = StudentSerializer(result, many=True)
+        return Response(serializer.data)
+
     
     def post(self,request):
         serial=StudentSerializer(data=request.data)
